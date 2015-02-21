6cf656285ae6188cca50605ed41c81a57b6141b0
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2015-02-21 21:58:46 +0100

    Added missing support files #3

--- Sources/SharedAssemblyInfo.cs
+++ Sources/SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Dennis Doomen 2015")]
 
-[assembly: AssemblyVersion("0.2.0.4")]
-[assembly: AssemblyFileVersion("0.2.0.4")]
-[assembly: AssemblyInformationalVersion("0.2.0-unstable.4+4.Branch.develop.Sha.6b3ff7db126b3e7bf89127644ee94144d935c071")]
+[assembly: AssemblyVersion("0.2.0.6")]
+[assembly: AssemblyFileVersion("0.2.0.6")]
+[assembly: AssemblyInformationalVersion("0.2.0-unstable.6+6.Branch.develop.Sha.0620fa1b35d92c7b9c2b164c7d3b8124d3308724")]
