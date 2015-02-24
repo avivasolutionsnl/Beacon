fe19c9d0b4429755124e14f3733cd384f97b310c
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2015-02-24 21:42:46 +0100

    Added a simple WPF app to test the supported devices without TeamCity

---
+++ Sources/Beacon.Lights.Tester/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Windows;
+
+using Beacon.Core;
+
+namespace Beacon.Lights.Tester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly LightFactory lightFactory = new LightFactory();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            deviceList.ItemsSource = lightFactory.SupportedDevices;
+            deviceList.SelectedItem = lightFactory.SupportedDevices.First();
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            Device.NoStatus();
+        }
+
+        private void SuccessButton_Click(object sender, RoutedEventArgs e)
+        {
+            Device.Success();
+        }
+
+        private void FailedButton_Click(object sender, RoutedEventArgs e)
+        {
+            Device.Fail();
+        }
+
+        private void InvestigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Device.Investigate();
+        }
+
+        private IBuildLight Device
+        {
+            get { return lightFactory.CreateLight((string) deviceList.SelectedItem); }
+        }
+    }
+}
--- Sources/Beacon.Lights/LightFactory.cs
+++ Sources/Beacon.Lights/LightFactory.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Beacon.Core;
 using Beacon.Lights.Console;
 using Beacon.Lights.Delcom;
@@ -7,20 +10,28 @@ namespace Beacon.Lights
 {
     public class LightFactory
     {
+        private readonly Dictionary<string, IBuildLight> devices = new Dictionary<string, IBuildLight>();
+
+        public LightFactory()
+        {
+            devices.Add("console", new ConsoleBuildLight());
+            devices.Add("delcom", new DelcomLight());
+        }
+
+        public string[] SupportedDevices
+        {
+            get { return devices.Keys.ToArray(); }
+        }
+
         public IBuildLight CreateLight(string deviceName)
         {
-            switch (deviceName.ToLower())
+            if (devices.ContainsKey(deviceName))
             {
-                case "console":
-                    return new ConsoleBuildLight();
-
-                case "delcom":
-                    return new DelcomLight();
-
-                default:
-                {
-                    throw new NotSupportedException(deviceName + " is not a support build device");
-                }
+                return devices[deviceName];
+            }
+            else
+            {
+                throw new NotSupportedException(deviceName + " is not a support build device");
             }
         }
     }
