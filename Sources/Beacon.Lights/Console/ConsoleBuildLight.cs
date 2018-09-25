0e1538326fc8f02b17bf8c16c48d3e21edb8c857
Marco Glasbergen <marco.glasbergen@gmail.com>
2018-09-25 14:07:53 +0200

    Improves Beacon logging to console output (#29)

--- Sources/Beacon.Lights/Console/ConsoleBuildLight.cs
+++ Sources/Beacon.Lights/Console/ConsoleBuildLight.cs
@@ -6,46 +6,39 @@ namespace Beacon.Lights.Console
 {
     internal class ConsoleBuildLight : IBuildLight
     {
-        private readonly ConsoleColor defaultForegroundColor;
-
-        public ConsoleBuildLight()
-        {
-            defaultForegroundColor = System.Console.ForegroundColor;
-        }
-
         public void Success()
         {
             System.Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Build is successful");
-            System.Console.ForegroundColor = defaultForegroundColor;
+            System.Console.ResetColor();
         }
 
         public void Investigate()
         {
             System.Console.ForegroundColor = ConsoleColor.Yellow;
             System.Console.WriteLine("Build is being investigated");
-            System.Console.ForegroundColor = defaultForegroundColor;
+            System.Console.ResetColor();
         }
 
         public void Fail()
         {
             System.Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine("Build has failed");
-            System.Console.ForegroundColor = defaultForegroundColor;
+            System.Console.ResetColor();
         }
 
         public void Fixed()
         {
             System.Console.ForegroundColor = ConsoleColor.DarkGreen;
             System.Console.WriteLine("Build is successful");
-            System.Console.ForegroundColor = defaultForegroundColor;
+            System.Console.ResetColor();
         }
 
         public void NoStatus()
         {
             System.Console.ForegroundColor = ConsoleColor.DarkGray;
             System.Console.WriteLine("Build status is unknown");
-            System.Console.ForegroundColor = defaultForegroundColor;
+            System.Console.ResetColor();
         }
     }
 }
