9dc86c10c5135c3d7025a6bcc8e7c3cdf1d87146
Joost Meijles <joost@meijles.com>
2020-07-09 10:40:02 +0200

    Use Nuke build

--- Build/Modules/IO/New-Directory.ps1
+++
@@ -1,9 +0,0 @@
-function New-Directory
-{
-	Param(
-	[parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
-	[string] $path
-	)
-	
-	mkdir $path -ErrorAction SilentlyContinue | out-null
-}
--- Build/Modules/IO/Remove-Directory.ps1
+++
@@ -1,7 +0,0 @@
-function Remove-Directory {
-	Param(
-	[parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
-	[string] $path
-	)
-  rd $path -recurse -force -ErrorAction SilentlyContinue | out-null
-}
